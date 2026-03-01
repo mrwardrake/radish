e9d2aefa96a49537ac7d03b1d0b58d59cb4fc693
mrwardrake <128748359+mrwardrake@users.noreply.github.com>
2026-03-01 10:08:47 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,27 @@
+const CACHE = 'rad-shrimp-v1';
+const ASSETS = [
+  './radioactive-shrimp.html',
+  './manifest.json'
+];
+
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open('rad-shrimp-v1').then(c =>
-    c.addAll(['/', '/index.html', '/manifest.json'])
-  ));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
 });
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
 });
